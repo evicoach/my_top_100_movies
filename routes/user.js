aa8fd91be329863427b2d17c81663a6f27503ff1
Evi Owhotemu <evicoach@gmail.com>
2023-05-13 18:42:52 +0100

    Updated unit tests

--- routes/user.js
+++
@@ -1,6 +0,0 @@
-const Router = require("express").Router;
-const router = Router();
-const userController = require("../app/users/user_controller");
-// router.get("/", userController.findByTag);
-// router.post("/waiting", userController.joinWaitingList);
-module.exports = router;
